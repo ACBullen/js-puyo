eef0ea507e80774c7bdc8b08a8eadf602f74242b
Alexander Bullen <acbullen1990@gmail.com>
2017-06-02 09:18:37 -0700

    Change file sourcing on bump audio

--- bundle.js
+++ bundle.js
@@ -98,7 +98,7 @@ class Board {
     this.activePuyo.childPuyo.AnimX = 140;
     this.activePuyo.childPuyo.AnimY = 20;
 
-    this.bump = new Audio('./assets/170141__timgormly__8-bit-bump.mp3');
+    this.bump = new Audio('https://raw.githubusercontent.com/myName/myRepo/master/vendor/assets/170141_timgormly_8-bit-bump.mp3');
 
     this.game.handleNewNext(this.puyoQueue[0]);
 
